3f7af7ac0eea952321216ebd7410a163088440ff
David Delvin <daviddelvin@Davids-iMac.lan>
2025-06-10 20:33:43 -0600

    Fix: Pass session to form on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers'
 import AIGenerateForm from "@/components/AIGenerateForm";
 import Header from "@/components/Header/page";
 
 
-export default function Home() {
+export default async function Home() {
+
+  const supabase = createServerComponentClient({ cookies });
+
+  // Get the initial user session on the server
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <main className="h-full">
       <Header />
@@ -10,7 +20,7 @@ export default function Home() {
         <h1 className="text-8xl md:text-6xl pb-3 text-center">Generate A Quote</h1>
         <p className="text-center text-base max-w-prose">This is a quote generator. You can select a person of interest that  this generator will replicate and also the tone in which you want the quote to be. Please give a description of the quote</p>
         <div className="mt-12">
-          <AIGenerateForm />
+          <AIGenerateForm session={session} />
         </div>
       </div>
     </main>
